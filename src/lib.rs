0c3ff88af6262535e80f99899ba73db2bd948c06
Marc Haubenstock <marc.dhaubenstock@gmail.com>
2023-02-11 00:50:16 +0100

    initial binding generation

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,25 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::mem;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn scs_flags() {
+        assert_eq!(SCS_INFEASIBLE_INACCURATE,-7);
+        assert_eq!(SCS_UNBOUNDED_INACCURATE,-6);
+        assert_eq!(SCS_SIGINT,-5);
+        assert_eq!(SCS_FAILED,-4);
+        assert_eq!(SCS_INDETERMINATE,-3);
+        assert_eq!(SCS_INFEASIBLE,-2);
+        assert_eq!(SCS_UNBOUNDED,-1);
+        assert_eq!(SCS_UNFINISHED,0);
+        assert_eq!(SCS_SOLVED,1);
+        assert_eq!(SCS_SOLVED_INACCURATE,2);
     }
 }
